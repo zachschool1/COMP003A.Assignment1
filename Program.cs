52245e8105b440475830718ae5adc5d596fbf15b
zachschool1 <zacharyjordan@my.whccd.edu>
2026-01-15 22:29:44 -0800

    first 10 about me written

--- Program.cs
+++ Program.cs
@@ -4,7 +4,116 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is 100 things about me!");
+            Console.WriteLine("#1 I am a father");
+            Console.WriteLine("#2 I have two cats");
+            Console.WriteLine("#3 I have two daughters");
+            Console.WriteLine("#4 I bought a laptop for sale just for this class");
+            Console.WriteLine("#5 My daughter broke my other laptop");
+            Console.WriteLine("#6 My old laptop was almost 10 years old");
+            Console.WriteLine("#7 I like JavaScript");
+            Console.WriteLine("#8 I hope c# is a good language to learn");
+            Console.WriteLine("#9 I want to learn how to simulate physics");
+            Console.WriteLine("#10 I am watching the Traitors season 4.");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
+
+            Console.WriteLine("#1 ");
+            Console.WriteLine("#2 ");
+            Console.WriteLine("#3 ");
+            Console.WriteLine("#4 ");
+            Console.WriteLine("#5 ");
+            Console.WriteLine("#6 ");
+            Console.WriteLine("#7 ");
+            Console.WriteLine("#8 ");
+            Console.WriteLine("#9 ");
+            Console.WriteLine("#10 ");
         }
     }
 }
